02315b107529c787637723fa30451679609146b3
Miki Tebeka <miki.tebeka@gmail.com>
2010-10-27 19:47:40 -0700

    Updated depenecies to clojure 1.2.0 and lein 1.3.1

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject lein-run "1.0.1-SNAPSHOT"
   :description "A leiningen plugin/task to call a function in a new process or 'run' a .clj file."
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]]
-  :dev-dependencies [[leiningen "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[leiningen "1.3.1"]
 		     [lein-clojars "0.6.0"]]
   :disable-implicit-clean true
   :run-aliases {:default [leiningen.run.test1 hello]
